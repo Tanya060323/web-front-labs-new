167e2996cc506ba195251ce5084a04c270a19f62
Татьяна Щегорцова <tttaaannnaaa62@gmail.com>
2025-05-09 03:48:57 +0700

    Правки

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон Дао и дэ',
+        publicationYear: 2022,
+        price: 460,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 505,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: 850,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 2023,
+        price: 386,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Рэй Брэдбери',
+        name: '451 градус по Фаренгейту',
+        publicationYear: 2023,
+        price: 491,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Оскар Уайльд',
+        name: 'Портрет Дориана Грея',
+        publicationYear: 2022,
+        price: 251,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Джейн Остин',
+        name: 'Гордость и предубеждение',
+        publicationYear: 2021,
+        price: 223,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Пилчер Розамунда',
+        name: 'Собиратели ракушек',
+        publicationYear: 2021,
+        price: 788,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Антуан де Сент-Экзюпери',
+        name: 'Маленький принц',
+        publicationYear: 2022,
+        price: 650,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Джойс Джеймс',
+        name: 'Улисс',
+        publicationYear: 2022,
+        price: 1074,
+        bestseller: false,
+        inStock: true
+    }
+];
