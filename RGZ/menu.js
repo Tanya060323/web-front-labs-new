b6e20ba71200deb525c15edaf3b9fa3abd15ece8
Татьяна Щегорцова <tttaaannnaaa62@gmail.com>
2025-05-28 04:19:03 +0700

    Удалены старые изображения, добавлены новые директории

---
+++ RGZ/menu.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const currentPageUrl = window.location.pathname.split('/').pop();
+    
+    const menuLinks = document.querySelectorAll('.main-menu a');
+    
+    menuLinks.forEach(link => {
+        const linkUrl = link.getAttribute('href');
+        
+        if (linkUrl === currentPageUrl) {
+            link.classList.add('current');
+        } else {
+            link.classList.remove('current');
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const backToTopButton = document.getElementById('backToTop');
+            
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset > 300) {
+            backToTopButton.style.display = 'flex';
+        } else {
+            backToTopButton.style.display = 'none';
+        }
+    });
+            
+    backToTopButton.addEventListener('click', function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
